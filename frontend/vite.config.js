b617b0f22802bc446effdb55d11b05a73461b9e3
Eysn0130 <103180203+Eysn0130@users.noreply.github.com>
2025-10-23 01:05:48 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   root: './frontend',  // 设置根目录为 frontend 文件夹
   plugins: [react()],
   build: {
-    outDir: '../dist',  // 设置构建输出目录为项目根目录下的 dist 文件夹
+    outDir: '../dist',  // 设置构建输出目录为根目录下的 dist 文件夹
   },
 });
