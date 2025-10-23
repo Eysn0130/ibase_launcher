662bc9921ade15df0b97c68f171586789d828fbb
Eysn0130 <103180203+Eysn0130@users.noreply.github.com>
2025-10-23 17:13:17 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ibase_wrapper.py/',  // GitHub Pages 部署的路径，确保与你的仓库名称一致
+  base: '/ibase_launcher/',  // GitHub Pages 部署的路径，确保与你的仓库名称一致
   plugins: [react()],
   build: {
     outDir: 'dist',  // 构建输出目录
